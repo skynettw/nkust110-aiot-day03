30604d7bc80afcad004d59984d5cb5a3d317daeb
Richard <skynet.tw@gmail.com>
2022-07-06 15:23:48 +0800

    sqlite to mysql ok

--- test16.py
+++ test16.py
@@ -1,7 +1,27 @@
 import sqlite3
+import MySQLdb
+
+mysql_conn = MySQLdb.connect(
+    host = "127.0.0.1",
+    port = 3307,
+    user = 'root',
+    passwd = '12345678',
+    db = 'nkustnews'
+)
+cursor = mysql_conn.cursor()
+cursor.execute("SET NAMES 'utf8'")
+cursor.execute("SET CHARACTER SET utf8")
 db = sqlite3.connect("nkustnews.db")
 sql = "select * from news;"
 rows = db.execute(sql)
 for row in rows:
-    print(row[1])
-        
+    try:
+        my_sql = "insert into news (title, content, date, url) values('{}','{}','{}','{}');".format(
+            row[1], row[2], row[3], row[4]
+        )
+        cursor.execute(my_sql)
+        mysql_conn.commit()
+    except:
+        pass
+db.close()
+mysql_conn.close()        
