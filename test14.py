0a41afe5f772b2da607b5b44a4eabce4c6376cc6
Richard <skynet.tw@gmail.com>
2022-07-06 10:29:17 +0800

    complete test13.py

--- test14.py
+++ test14.py
@@ -1,1 +1,9 @@
-#Hello for third day~~
+import sqlite3
+db = sqlite3.connect("nkustnews.db")
+schools = ['台科', '北科', '雲科', '屏科', '中科']
+for school in schools:
+    sql = "select count(*) from news where content like '%{}%';".format(school)
+    rows = db.execute(sql)
+    for row in rows:
+        #print(school, ":", row[0], "次")
+        print("{}:{}次".format(school, row[0]))
